6b94b685d2e139f8444ee143c97cc4158387985a
jykim3097 <jykim3097@gmail.com>
2021-05-17 23:31:27 +0900

    답안 제출 1. 어렵게 생각하지 않기- 범위를 나누어 생각하자 2. System.in.read() 함수를 알게 되었다- 아스키코드와 개행문자만 받아오는 함수

--- src/stepbystep/step7/string/_8_5622.java
+++ src/stepbystep/step7/string/_8_5622.java
@@ -12,10 +12,46 @@ public class _8_5622 {
 		 * 
 		 */
 		
-		BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
-		String s = rd.readLine();
+		//1차 답안
+//		BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+//		String s = rd.readLine();
+//	
+//		int n = s.length();
+//		
+//		for(int i=0; i<s.length(); i++) {
+//	
+//			if(s.charAt(i) == 'A' || s.charAt(i) == 'B' || s.charAt(i) == 'C') n += 2;
+//			if(s.charAt(i) == 'D' || s.charAt(i) == 'E' || s.charAt(i) == 'F') n += 3;
+//			if(s.charAt(i) == 'G' || s.charAt(i) == 'H' || s.charAt(i) == 'I') n += 4;
+//			if(s.charAt(i) == 'J' || s.charAt(i) == 'K' || s.charAt(i) == 'L') n += 5;
+//			if(s.charAt(i) == 'M' || s.charAt(i) == 'N' || s.charAt(i) == 'O') n += 6;
+//			if(s.charAt(i) == 'P' || s.charAt(i) == 'Q' || s.charAt(i) == 'R' || s.charAt(i) == 'S') n += 7;
+//			if(s.charAt(i) == 'T' || s.charAt(i) == 'U' || s.charAt(i) == 'V') n += 8;
+//			if(s.charAt(i) == 'W' || s.charAt(i) == 'X' || s.charAt(i) == 'Y' || s.charAt(i) == 'Z') n += 9;
+//
+//		}
+//		
+//		System.out.println(n);
 		
-		//double a = Math.(((double)s.charAt(0)));
-		//System.out.println(a);
+		
+		//2차답안
+		int num= System.in.read(); //아스키코드 값을 받아온다
+		int sum = 0;
+		
+		while(num >= 65 && num <=90) {
+			
+			if( num < 68) sum+=3;
+			else if (num < 71) sum+=4;
+			else if (num < 74) sum+=5;
+			else if (num < 77) sum+=6;
+			else if (num < 80) sum+=7;
+			else if (num < 84) sum+=8;
+			else if (num < 87) sum+=9;
+			else sum+=10;
+			
+			num = System.in.read();
+		}
+		
+		System.out.println(sum);
 	}
 }
