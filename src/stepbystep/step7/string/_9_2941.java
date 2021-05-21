e608b25c6e1f3c376332559e8124915ac2964026
jykim3097 <jykim3097@gmail.com>
2021-05-21 16:41:41 +0900

    아이디어 구상 중

---
+++ src/stepbystep/step7/string/_9_2941.java
@@ -0,0 +1,18 @@
+package stepbystep.step7.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _9_2941 {
+
+	public static void main(String[] args) throws IOException {
+		String[] cro = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		
+		int count = 0;
+
+	}
+}
