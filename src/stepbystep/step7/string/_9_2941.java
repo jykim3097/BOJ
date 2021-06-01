05db1e60b9e37048b4f7577a6f30e2420910df11
jiyoung <jykim3097@gmail.com>
2021-06-01 17:02:56 +0900

    같은 값이 들어왔을 대 처리 아이디어 구상 중

--- src/stepbystep/step7/string/_9_2941.java
+++ src/stepbystep/step7/string/_9_2941.java
@@ -7,12 +7,20 @@ import java.io.InputStreamReader;
 public class _9_2941 {
 
 	public static void main(String[] args) throws IOException {
+		
+		//https://www.acmicpc.net/problem/2941
+		
 		String[] cro = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
 		
-		int count = 0;
+		for(int i=0; i<cro.length; i++) {
+			if(str.contains(cro[i])) {
+				str = str.replace(cro[i], "  ");
+			}
+		}
 
+		System.out.println(str);
 	}
 }
