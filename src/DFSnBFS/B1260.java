f6c9d8e59562236de406178e1ba5e7709da68cce
jykim3097 <jykim3097@gmail.com>
2021-09-08 22:39:27 +0900

    210904 dfs 공부

---
+++ src/DFSnBFS/B1260.java
@@ -0,0 +1,30 @@
+package DFSnBFS;
+
+import java.util.LinkedList;
+
+public class B1260 {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+}
+
+
+class Graph {
+	
+	private int V; // 노드의 개수
+	private LinkedList<Integer> adj; // 인접 리스트
+	
+	public Graph(int v) {
+		V = v;
+		adj = new LinkedList<>();
+		
+		for(int i = 0; i<v; i++) {
+			
+		}
+	}
+	
+	
+}
